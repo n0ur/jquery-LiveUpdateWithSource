82d946a6df53417e4d8bb49be7692d5a6a488807
nour <nour.fwh@gmail.com>
2013-03-22 16:35:47 +0200

    inital commit

---
+++ script/jquery.livesearchwithsource.js
@@ -0,0 +1,30 @@
+jQuery.fn.liveUpdateWithSource = function(list, source){
+	var that = this;
+	// $.post(source).done(function( res ) { 
+		list = $(list);
+		var ohtml = list.html(),
+			// jsonData = res, 
+			jsonData = source;
+
+		that.keyup(filter).keyup().parents('form').submit(function(){ return false; });
+
+		return that;
+
+		function filter(){
+			var term = jQuery.trim( jQuery(this).val().toLowerCase() ), scores = [], html = "";
+			if ( term ) {
+				jQuery.each(jsonData, function(i){
+					var score = this.name.toLowerCase().score(term);
+
+					if (score > 0) { scores.push([score, i]); }
+				});
+				jQuery.each(scores.sort(function(a, b){return b[0] - a[0];}), function(){
+					html+='<li>'+ jsonData[this[1]].name +'</li>';
+				});
+				list.html(html);
+			} else {
+				list.html(ohtml);
+			}
+		}
+	// });
+};
